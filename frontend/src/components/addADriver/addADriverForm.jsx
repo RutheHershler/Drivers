cacf2a86ab434e6face0fae9e92080ff24e2d32a
RutheHershler <N6786967@gmail.com>
2024-09-08 13:44:38 +0300

    driver-dialog

--- frontend/src/components/addADriver/addADriverForm.jsx
+++ frontend/src/components/addADriver/addADriverForm.jsx
@@ -1,12 +1,62 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 
-export default function FullWidthTextField() {
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import { TextField } from '@mui/material';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+
+export default function CustomizedDialogs() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <Box sx={{ width: 500, maxWidth: '100%' }}>
-            <TextField  label="שם" id="name" />
+        <React.Fragment>
+            <Button variant="outlined" onClick={handleClickOpen}>
+                להצטרפות כנהג לחברה
+            </Button>
+            <BootstrapDialog
+                onClose={handleClose}
+                aria-labelledby="customized-dialog-title"
+                open={open}
+            >
+                <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+                    Modal title
+                </DialogTitle>
+                <IconButton
+                    aria-label="close"
+                    onClick={handleClose}
+                    sx={(theme) => ({
+                        position: 'absolute',
+                        right: 8,
+                        top: 8,
+                        color: theme.palette.grey[500],
+                    })}
+                >
+                    <CloseIcon />
+                </IconButton>
+                <DialogContent dividers>
+                <TextField  label="שם" id="name" />
             <TextField  label="מספר טלפון" id="phon-number" />
             <TextField  label="מספר ווצאפ" id="watsap-number" />
             <TextField  label="כתובת" id="address" />
@@ -14,8 +64,10 @@ export default function FullWidthTextField() {
             <Button variant="contained" disableElevation>
                 שלח למנהל
             </Button>
-        </Box>
+
+                </DialogContent>
+
+            </BootstrapDialog>
+        </React.Fragment>
     );
 }
-
-
