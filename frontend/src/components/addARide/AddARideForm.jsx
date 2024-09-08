fb25664e0dba95ed559f5c122bbd0b7247c41e39
ChaniRoz <m05276539232@GMAIL.COM>
2024-09-08 15:35:31 +0300

    create basic redux and slices

--- frontend/src/components/addARide/AddARideForm.jsx
+++ frontend/src/components/addARide/AddARideForm.jsx
@@ -1,20 +1,72 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { Box } from '@mui/material';
-
+import Box from '@mui/material/Box';
+import { addRide } from '../../redux/rideSlice';
 
 export default function AddARideForm() {
+    const dispatch = useDispatch();
+    const [rideData, setRideData] = useState({
+        exitLocation: '',
+        destinationLocation: '',
+        numberOfPassengers: 1,
+        phoneNumber: '',
+    });
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setRideData((prevRideData) => ({
+            ...prevRideData,
+            [name]: value,
+        }));
+    };
+
+    const handleAddRide = () => {
+        dispatch(addRide(rideData));
+    };
+
     return (
         <>
             <Box sx={{ width: 500, maxWidth: '100%' }}>
-                <TextField fullWidth type="text" label="מיקום יציאה" id="exit-location" />
-                <TextField fullWidth type="text" label="מיקום יעד" id="destination-location" />
-                <TextField fullWidth type="number" label="מספר נוסעים" id="number-of-passengers" />
-                <TextField fullWidth type="text" label="מספר פלאפון" id="phone-number" />
+                <TextField
+                    fullWidth
+                    type="text"
+                    label="מיקום יציאה"
+                    id="exit-location"
+                    name="exitLocation"
+                    value={rideData.exitLocation}
+                    onChange={handleInputChange}
+                />
+                <TextField
+                    fullWidth
+                    type="text"
+                    label="מיקום יעד"
+                    id="destination-location"
+                    name="destinationLocation"
+                    value={rideData.destinationLocation}
+                    onChange={handleInputChange}
+                />
+                <TextField
+                    fullWidth
+                    type="number"
+                    label="מספר נוסעים"
+                    id="number-of-passengers"
+                    name="numberOfPassengers"
+                    value={rideData.numberOfPassengers}
+                    onChange={handleInputChange}
+                />
+                <TextField
+                    fullWidth
+                    type="text"
+                    label="מספר פלאפון"
+                    id="phone-number"
+                    name="phoneNumber"
+                    value={rideData.phoneNumber}
+                    onChange={handleInputChange}
+                />
             </Box>
-            <Button variant="contained" disableElevation>
+            <Button variant="contained" disableElevation onClick={handleAddRide}>
                 פרסם הודעה
             </Button>
         </>
---
+++ frontend/src/redux/rideSlice.jsx
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Ride from '../classes/ride'
+import UsePost from './hooks/postHook';
+
+const initialRideState = {
+    rides: [],
+};
+
+const rideSlice = createSlice({
+    name: "ride",
+    initialState: initialRideState,
+    reducers: {
+        addRide: (state, action) => {
+            const rideData = action.payload;
+            const rideInstance = {
+                exitLocation: rideData.exitLocation,
+                destinationLocation: rideData.destinationLocation,
+                numberOfPassengers: rideData.numberOfPassengers,
+                phoneNumber: rideData.phoneNumber,
+            };
+            // Make the API call to post the data to the server
+            // const post = UsePost();
+            // post('http://localhost:3000/rides', rideInstance);
+
+            // Save the ride data locally in the state
+            state.rides.push(rideInstance);
+            console.log(rideInstance);
+
+        },
+    },
+});
+
+export const { addRide } = rideSlice.actions;
+export const selectRides = (state) => state.rides;
+export default rideSlice.reducer;
